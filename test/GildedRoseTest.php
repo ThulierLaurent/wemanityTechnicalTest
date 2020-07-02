3e0fe59fadbc60575e77a4d4b81ebc1fd303a44e
Thulier Laurent <thulierlaurent@mbp-de-thulier.home>
2020-07-02 17:00:10 +0200

    composer install for phpunit + correction

--- test/GildedRoseTest.php
+++ test/GildedRoseTest.php
@@ -3,10 +3,11 @@
 namespace App;
 
 class GildedRoseTest extends \PHPUnit\Framework\TestCase {
+
     public function testFoo() {
         $items      = [new Item("foo", 0, 0)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertEquals("fixme", $items[0]->name);
+        $this->assertEquals("foo", $items[0]->name);
     }
 }
