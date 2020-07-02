62d31da3ecbfadabe55fb1e9d16b6295407af8d9
Thulier Laurent <thulierlaurent@mbp-de-thulier.home>
2020-07-02 18:27:35 +0200

    Backstage Passes (not after concert) -  updateQuality

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -5,6 +5,7 @@ namespace App;
 final class GildedRose {
 
     private $items = [];
+    private $maxValue = 50;
 
     public function __construct($items) {
         $this->items = $items;
@@ -12,7 +13,7 @@ final class GildedRose {
 
     public function updateQuality() {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($item->quality > 0) {
                     if ($item->name != 'Sulfuras, Hand of Ragnaros') {
                         if ($item->name == 'Conjured Mana Cake') {
@@ -22,18 +23,19 @@ final class GildedRose {
                         }
                     }
                 }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+            } 
+            else {
+                if ($item->quality < $this->maxValue) {
+                    $item->quality += 1;
+                    if ($item->name == 'Backstage passes') {
                         if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
+                            if ($item->quality < $this->maxValue) {
+                                $item->quality += 1;
                             }
                         }
                         if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
+                            if ($item->quality < $this->maxValue) {
+                                $item->quality +=  1;
                             }
                         }
                     }
@@ -46,7 +48,7 @@ final class GildedRose {
             
             if ($item->sell_in < 0) {
                 if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->name != 'Backstage passes' or $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->quality > 0) {
                             if ($item->name != 'Sulfuras, Hand of Ragnaros') {
                                 $item->quality = $item->quality - 1;
@@ -57,8 +59,8 @@ final class GildedRose {
                         //drop to 0 after a concert
                         $item->quality = 0;
                     }
-                } else {
-                    if ($item->quality < 50) {
+                }else {
+                    if ($item->quality < $this->maxValue) {
                         $item->quality = $item->quality + 1;
                     }
                 }
--- test/GildedRoseTest.php
+++ test/GildedRoseTest.php
@@ -44,15 +44,17 @@ class GildedRoseTest extends \PHPUnit\Framework\TestCase {
 
     //Tests - Passes en coulisses
     public function testBackstagePassesProduct(){
-    	$items      = [new Item("Backstage passes", 10, 30)];
+    	$valueQuality = 30;
+    	$items      = [new Item("Backstage passes", 10, $valueQuality)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertEquals(60, $items[0]->quality*2);
+        $this->assertEquals($valueQuality+2, $items[0]->quality+2);
 
-        $items      = [new Item("Backstage passes", 4, 90)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertEquals(90, $items[0]->quality*3);
+        $valueQuality2 = 30;
+        $items2      = [new Item("Backstage passes", 5, $valueQuality2)];
+        $gildedRose2 = new GildedRose($items);
+        $gildedRose2->updateQuality();
+        $this->assertEquals($valueQuality2+3, $items[0]->quality+3);
     }
 
     //Conjured
